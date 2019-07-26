7724ecfdf5405f0ca8fe520458ef62fd1551906b
박주완 <wannypark@codecraft.co.kr>
2019-07-26 18:41:14 +0900

    Init Commit

--- Package.swift
+++ Package.swift
@@ -5,8 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ActionKit",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ActionKit",
             targets: ["ActionKit"]),
--- Sources/ActionKit/ActionKit.swift
+++ Sources/ActionKit/ActionKit.swift
@@ -1,3 +1,27 @@
-struct ActionKit {
-    var text = "Hello, World!"
+import UIKit
+
+final class Button: UIButton {
+    
+    typealias DidTapButton = (Button) -> ()
+    
+    public var didTouchUpInside: DidTapButton? {
+        
+        didSet {
+            if didTouchUpInside != nil {
+                addTarget(self, action: #selector(didTouchUpInside(sender:)), for: .touchUpInside)
+            }
+            else {
+                removeTarget(self, action: #selector(didTouchUpInside(sender:)), for: .touchUpInside)
+            }
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func didTouchUpInside(sender: UIButton) {
+        
+        if let handler = didTouchUpInside {
+            handler(self)
+        }
+    }
 }
